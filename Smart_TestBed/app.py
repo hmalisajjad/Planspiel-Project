45eb1aabc22d70a9f64e052d1dde8679f1c42554
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2022-04-06 12:17:39 +0200

    connection with frontend by api

---
+++ Smart_TestBed/app.py
@@ -0,0 +1,58 @@
+# Reference from: https://github.com/bradnielsen2981/grovepi-flask
+
+from flask import Flask, render_template, jsonify, request
+import logging #allow loggings
+import grove #imports the grove functionality that you define
+import ipaddress #imports the display on I2C-2
+from datetime import datetime
+
+#uses JSONIFY to encode data structures in Strings. AJAX can then change it back..
+#Global Variables
+app = Flask(__name__)
+log = app.logger #sets up a log -- to log call log.info('message')
+#log.error("Testing") --use this to log an error
+
+#request handlers ---------------------------------------------
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+#start a light
+@app.route('/startled', methods=['GET','POST'])
+def startled():
+    grove.turn_on_led_digitalport(4) #turn on led on d4
+    return jsonify({ "message":"starting" }) #jsonify take any type and makes a JSON string
+
+#stop a light
+@app.route('/stopled', methods=['GET','POST'])
+def stopled():
+    grove.turn_off_led_digitalport(4) #turn off led on d4
+    return jsonify({ "message":"stopping" })
+
+
+
+
+#return light values
+@app.route('/getlight', methods=['GET','POST'])
+def getlight():
+    light = grove.read_light_sensor_analogueport(1) #get light sensor from a1
+    return jsonify({ "Light":light })
+
+#return temp values
+@app.route('/gettemphumidity', methods=['GET','POST'])
+def gettemphumidity():
+    [temp,humidity] = grove.read_temp_humidity_sensor_digitalport(3) #get temperature and humidity
+    return jsonify({ "Temperature":temp, "Humidity":humidity })
+
+
+#---------------------------------------------------------------
+#Shutdown the web server
+@app.route('/shutdown', methods=['GET','POST'])
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    func()
+    return jsonify({ "message":"shutting down" }) 
+
+#Threaded mode is important if using shared resources e.g. sensor, each user request launches a thread.. 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
