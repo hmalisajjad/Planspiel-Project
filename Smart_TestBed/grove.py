cfcbcbeb35b7038bcbd130d0b0fa3399bacaa455
Asad Sujon <md-asaduzzaman.sujon@s2019.tu-chemnitz.de>
2022-04-06 12:14:12 +0200

    Add components fucntions

---
+++ Smart_TestBed/grove.py
@@ -0,0 +1,48 @@
+''' This file will provide an interface to your GrovePi code. The functions should accept the port, so to make them port independent. '''
+# Reference from: https://github.com/bradnielsen2981/grovepi-flask
+import grovepi
+import time
+import logging
+
+log = logging.getLogger('app')
+
+# This function will return the current light reading from the desired ANALOG port A0, A1 etc
+def read_light_sensor_analogueport(port):
+    light_sensor = port
+    grovepi.pinMode(light_sensor,"INPUT")
+    sensor_value = None
+    try:
+        sensor_value = grovepi.analogRead(light_sensor) # Get sensor value
+    except IOError: #this doesnt appear to work
+        log.error("Error in reading the light sensor")
+    return sensor_value
+
+
+#Turn on the led
+def turn_on_led_digitalport(port):
+    led = port
+    grovepi.pinMode(led,"OUTPUT")
+    grovepi.digitalWrite(led,255)
+    return
+
+#Turn off the led
+def turn_off_led_digitalport(port):
+    led = port
+    grovepi.pinMode(led,"OUTPUT")
+    grovepi.digitalWrite(led,0)
+    return
+
+
+#read temp and humidity
+def read_temp_humidity_sensor_digitalport(port):
+    tempsensor = port
+    grovepi.pinMode(tempsensor,"INPUT")
+    temp_humidity_list = None
+    try:
+        temp_humidity_list = grovepi.dht(port,0) #0 - type blue sensor
+    except IOError: #this doesnt appear to work
+        log.error("Error in reading the temp and humidity sensor")
+    return temp_humidity_list
+
+#--------------------------------------------------------------------
+
